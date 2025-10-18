91dd6235236fd4d290b3df94c0fa757069921b69
ocjdagreat1 <oparachibuikejustine@gmail.com>
2025-10-17 23:35:02 -0800

    new

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,36 +15,32 @@ export default function ServiceSection() {
       offset: 100,
     });
 
-    // Fetch data from API
+    // Fetch data
     axios
-      .get("https://jsonplaceholder.typicode.com/posts?_limit=18")
+      .get("https://jsonplaceholder.typicode.com/posts?_limit=9")
       .then((res) => setData(res.data))
       .catch((err) => console.error("Error fetching data:", err));
   }, []);
 
-  // Split data into 6 chunks (3 cards each)
+  // Split data into 3 chunks
   const chunk1 = data.slice(0, 3);
   const chunk2 = data.slice(3, 6);
   const chunk3 = data.slice(6, 9);
-  const chunk4 = data.slice(9, 12);
-  const chunk5 = data.slice(12, 15);
-  const chunk6 = data.slice(15, 18);
 
   const getAosType = (index) => {
     const directions = ["fade-up", "fade-right", "fade-left", "zoom-in"];
     return directions[index % directions.length];
   };
 
-  // Random tech/media image generator
+  // Random tech image generator
   const getImage = (index) =>
-    `https://picsum.photos/seed/tech${index}/400/250`;
+    `https://picsum.photos/seed/tech${index}/400/250?grayscale`;
 
   return (
     <div className="services-wrapper">
-
       {/* SECTION 1 */}
       <section className="service-container theme-blue" data-aos="fade-up">
-        <h2 className="service-title">Web Development & Engineering</h2>
+        <h2 className="service-title">Wonderland</h2>
         {chunk1.map((item, index) => (
           <div
             key={item.id}
@@ -62,7 +58,7 @@ export default function ServiceSection() {
 
       {/* SECTION 2 */}
       <section className="service-container theme-orange" data-aos="fade-right">
-        <h2 className="service-title">Data & Analytics</h2>
+        <h2 className="service-title">Magnificient Land</h2>
         {chunk2.map((item, index) => (
           <div
             key={item.id}
@@ -80,7 +76,7 @@ export default function ServiceSection() {
 
       {/* SECTION 3 */}
       <section className="service-container theme-purple" data-aos="fade-left">
-        <h2 className="service-title">Creative Media</h2>
+        <h2 className="service-title">Harmonous Land</h2>
         {chunk3.map((item, index) => (
           <div
             key={item.id}
@@ -95,61 +91,6 @@ export default function ServiceSection() {
           </div>
         ))}
       </section>
-
-      {/* SECTION 4 */}
-      <section className="service-container theme-red" data-aos="fade-up">
-        <h2 className="service-title">Digital Marketing</h2>
-        {chunk4.map((item, index) => (
-          <div
-            key={item.id}
-            className="service"
-            data-aos={getAosType(index + 3)}
-            data-aos-delay={index * 150}
-          >
-            <img src={getImage(index + 9)} alt="Marketing" className="service-img" />
-            <i className="fa-solid fa-bullhorn icon-red"></i>
-            <h3>{item.title.slice(0, 35)}...</h3>
-            <p>{item.body.slice(0, 100)}...</p>
-          </div>
-        ))}
-      </section>
-
-      {/* SECTION 5 */}
-      <section className="service-container theme-teal" data-aos="fade-right">
-        <h2 className="service-title">Video Editing</h2>
-        {chunk5.map((item, index) => (
-          <div
-            key={item.id}
-            className="service"
-            data-aos={getAosType(index + 4)}
-            data-aos-delay={index * 150}
-          >
-            <img src={getImage(index + 12)} alt="Video" className="service-img" />
-            <i className="fa-solid fa-video icon-teal"></i>
-            <h3>{item.title.slice(0, 35)}...</h3>
-            <p>{item.body.slice(0, 100)}...</p>
-          </div>
-        ))}
-      </section>
-
-      {/* SECTION 6 */}
-      <section className="service-container theme-green" data-aos="fade-left">
-        <h2 className="service-title">Graphics Design</h2>
-        {chunk6.map((item, index) => (
-          <div
-            key={item.id}
-            className="service"
-            data-aos={getAosType(index + 5)}
-            data-aos-delay={index * 150}
-          >
-            <img src={getImage(index + 15)} alt="Graphics" className="service-img" />
-            <i className="fa-solid fa-pen-nib icon-green"></i>
-            <h3>{item.title.slice(0, 35)}...</h3>
-            <p>{item.body.slice(0, 100)}...</p>
-          </div>
-        ))}
-      </section>
-
     </div>
   );
 }
